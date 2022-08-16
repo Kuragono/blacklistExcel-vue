ff0b8832d6e086c9150920742c1fb7fca3277870
Kuragono <tiago.carreira@outlook.com>
2022-08-16 15:38:50 +0200

    Create App and Components

---
+++ Blacklist/src/components/ActivateButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <button @click.prevent="clickOut(SELECTOR1, URL1)">Artikel</button>
+        <button @click.prevent="clickOut(SELECTOR2, URL2)">Artikel Mobile</button>
+        <button @click.prevent="clickOut(SELECTOR3, URL3)">Beipack</button>
+        <button @click.prevent="clickOut(SELECTOR4, URL4)">Beipack Mobile</button>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                SELECTOR1: `#dm_conative_artikel`,
+                URL1: 'https://cc.conative.de/#/adslots/1493/positioning/3155',
+                SELECTOR2: `#dm_conative_artikel_mobile`,
+                URL2: `https://cc.conative.de/#/adslots/1493/positioning/3156`,
+                SELECTOR3: `#dm_conative_beipack`,
+                URL3: 'https://cc.conative.de/#/adslots/1493/positioning/3157',
+                SELECTOR4: `#dm_conative_beipack_mobile`,
+                URL4: 'https://cc.conative.de/#/adslots/1493/positioning/3158'
+            }
+        },
+        methods: {
+            clickOut(selector, url) {
+                
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
--- Blacklist/src/main.js
+++ Blacklist/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+
+import ActivateButton from './components/ActivateButton.vue';
+import GetData from './components/GetData.vue';
+import App from './App.vue';
 
 const app = createApp(App);
+
+app.component('activate-button', ActivateButton);
+app.component('get-data', GetData);
 
 app.mount("#app");
